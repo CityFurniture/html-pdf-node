e44014dde0cc9c11f9dfc68c43c8d9751a928008
Aian <aiana@city-furniture.com>
2023-11-06 14:20:15 +0000

    fix: refactor and added close page to Puppeteer

--- index.js
+++ index.js
@@ -1,94 +1,117 @@
 const puppeteer = require('puppeteer');
-var Promise = require('bluebird');
-const hb = require('handlebars')
-const inlineCss = require('inline-css')
-module.exports
-async function generatePdf(file, options, callback) {
-  // we are using headless mode
-  let args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-  ];
-  if(options.args) {
-    args = options.args;
-    delete options.args;
-  }
+const handlebars = require('handlebars');
+const inlineCss = require('inline-css');
+const Promise = require('bluebird');
 
-  const browser = await puppeteer.launch({
-    args: args
-  });
-  const page = await browser.newPage();
-
-  if(file.content) {
-    data = await inlineCss(file.content, {url:"/"});
-    console.log("Compiling the template with handlebars")
-    // we have compile our code with handlebars
-    const template = hb.compile(data, { strict: true });
-    const result = template(data);
-    const html = result;
-
-    // We set the page content as the generated html by handlebars
-    await page.setContent(html, {
-      waitUntil: 'networkidle0', // wait for page to load completely
-    });
+const DEFAULT_PUPPETEER_ARGS = [
+  '--no-sandbox',
+  '--disable-setuid-sandbox',
+];
+/**
+ * private function that launches a puppeteer browser. 
+ * @param {*} customArgs 
+ * @returns 
+ */
+async function _launchBrowser(customArgs = []) {
+  const args = customArgs.length > 0 ? customArgs : DEFAULT_PUPPETEER_ARGS;
+  return puppeteer.launch({ args });
+}
+/**
+ * private function that compiles the template with handlebars.
+ * @param {*} content 
+ * @returns 
+ */
+async function _compileTemplate(content) {
+  console.log("Compiling the template with handlebars");
+  const styledContent = await inlineCss(content, { url: "/" });
+  const template = handlebars.compile(styledContent, { strict: true });
+  return template(styledContent);
+}
+/**
+ * private function that sets the pdf content.
+ * @param {*} page
+ * @param {*} file
+ * @returns
+ *  */
+async function _setPdfContent(page, file) {
+  if (file.content) {
+    const html = await _compileTemplate(file.content);
+    await page.setContent(html, { waitUntil: 'networkidle0' });
   } else {
-    await page.goto(file.url, {
-      waitUntil:[ 'load', 'networkidle0'], // wait for page to load completely
-    });
+    await page.goto(file.url, { waitUntil: ['load', 'networkidle0'] });
   }
-
-  return Promise.props(page.pdf(options))
-    .then(async function(data) {
-       await browser.close();
-
-       return Buffer.from(Object.values(data));
-    }).asCallback(callback);
 }
 
-async function generatePdfs(files, options, callback) {
-  // we are using headless mode
-  let args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-  ];
-  if(options.args) {
-    args = options.args;
+/**
+ * This function closes all pages in the browser. It is used to avoid memory leaks. 
+ * @param {puppeteer.Puppeteer.browser} browser 
+*/
+async function _closeAllPages(browser) {
+  const pages = await browser.pages();
+  await Promise.all(pages.map(page => page.close()));
+}
+
+
+/**
+ * Generates a pdf from a file. 
+ * @param {*} file 
+ * @param {*} options 
+ * @param {callback} callback 
+ * @returns 
+ */
+async function generatePdf(file, options, callback) {
+  const browser = await _launchBrowser(options.args);
+  try {
     delete options.args;
+
+    const page = await browser.newPage();
+    await _setPdfContent(page, file);
+
+    const buffer = await page.pdf(options);
+    const result = Buffer.from(Object.values(buffer));
+    return Promise.resolve(result).asCallback(callback);
+  } catch (error) {
+    return Promise.reject(error).asCallback(callback);
+  } finally {
+    await _closeAllPages(browser);
+    await browser.close();
   }
-  const browser = await puppeteer.launch({
-    args: args
-  });
-  let pdfs = [];
-  const page = await browser.newPage();
-  for(let file of files) {
-    if(file.content) {
-      data = await inlineCss(file.content, {url:"/"})
-      console.log("Compiling the template with handlebars")
-      // we have compile our code with handlebars
-      const template = hb.compile(data, { strict: true });
-      const result = template(data);
-      const html = result;
-      // We set the page content as the generated html by handlebars
-      await page.setContent(html, {
-        waitUntil: 'networkidle0', // wait for page to load completely
-      });
-    } else {
-      await page.goto(file.url, {
-        waitUntil: 'networkidle0', // wait for page to load completely
+}
+/**
+ * Generates an array of pdfs from an array of files. 
+ * @param {*} files 
+ * @param {*} options 
+ * @param {callback} callback 
+ * @returns 
+ */
+async function generatePdfs(files, options, callback) {
+  const browser = await _launchBrowser(options.args);
+  try {
+    delete options.args;
+
+    const page = await browser.newPage();
+    const pdfs = [];
+
+    for (const file of files) {
+      await _setPdfContent(page, file);
+      const buffer = await page.pdf(options);
+      pdfs.push({
+        ...file,
+        buffer: Buffer.from(Object.values(buffer))
       });
     }
-    let pdfObj = JSON.parse(JSON.stringify(file));
-    delete pdfObj['content'];
-    pdfObj['buffer'] = Buffer.from(Object.values(await page.pdf(options)));
-    pdfs.push(pdfObj);
-  }
 
-  return Promise.resolve(pdfs)
-    .then(async function(data) {
-       await browser.close();
-       return data;
-    }).asCallback(callback);
+    return Promise.resolve(pdfs).asCallback(callback);
+  } catch (error) {
+    return Promise.reject(error).asCallback(callback);
+  } finally {
+    await _closeAllPages(browser);
+    await browser.close();
+  }
 }
 
-module.exports.generatePdf = generatePdf;
-module.exports.generatePdfs = generatePdfs;
+
+module.exports = {
+  generatePdf,
+  generatePdfs
+};
